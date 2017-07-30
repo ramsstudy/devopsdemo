4c0474da47a14ac9e5fe776d2031614abef01289
ramsstudy <ramsstudy@gmail.com>
2017-07-30 17:51:26 +0000

    Update HelloWorld.java
    Made change to test polling SCM funtion from jenkins

--- demoDevopsProj/src/com/devopsdemo/HelloWorld.java
+++ demoDevopsProj/src/com/devopsdemo/HelloWorld.java
@@ -2,6 +2,6 @@ package com.devopsdemo;
 public class HelloWorld {
        public static void main(String[] args) {
       
-             System.out.println(" Hello World - From Github ");
+             System.out.println(" Hello World - From Github  === > Tested for Polling SCM functionality *****");
                 }
            }
